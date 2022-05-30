c1184696108321744124d60996c24a26e032facc
ghanitabish <ghani.tabish@outlook.com>
2022-05-30 08:51:14 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FirstmoduleModule } from './firstmodule/firstmodule.module';
+import { EmployeeModule } from './employee/employee.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FirstmoduleModule,
+    EmployeeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
