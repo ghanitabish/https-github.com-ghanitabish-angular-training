85e27df27e2de99e30689d37eff1ae91a3cefdf6
ghanitabish <ghani.tabish@outlook.com>
2022-05-30 14:36:50 -0400

    Adding Routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Router1Component } from './routers/router1/router1.component';
+import { Router2Component } from './routers/router2/router2.component';
+import { HomeComponent } from './routers/home/home.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(
+    [
+      {
+        path: '',
+          redirectTo: 'home',
+          pathMatch: 'full',
+      },
+      {
+        path: 'router1',
+        component: Router1Component,
+      },
+      {
+        path: 'router2',
+        component: Router2Component,
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'home',
+      },
+    ],
+    { enableTracing: false }
+  ),
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { ButtonComponent } from './component/button/button.component';
 import { HomeComponent } from './component/home/home.component';
 import { ContactComponent } from './component/contact/contact.component';
+import { Router1Component } from './routers/router1/router1.component';
+import { Router2Component } from './routers/router2/router2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    Router1Component,
+    Router2Component
   ],
   imports: [
     BrowserModule,
